698fdc9e5ff620b139c0cb8a3ddc89f500baaab0
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2021-05-24 16:44:53 -0700

    reload when config file modified

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,6 +3,17 @@ set -e
 
 dnsmasq --test
 
+# RELOAD IF CONFIG FILE WAS MODIFIED
+CONFIG_FILE="/etc/dnsmasq.conf"
+while inotifywait -e close_write ${CONFIG_FILE} >/dev/null; do
+	if ! dnsmasq --test >/dev/null; then
+		echo "Not reloading config since dnsmasq config is invalid"
+	else
+		(ps -ef | grep [d]nsmasq | awk '{print $1}' | xargs kill -1) && echo "Reloaded config"
+	fi
+done &
+
+# RELOAD IF HOSTS FILE WAS MODIFIED
 HOSTS_FILE=$(grep '^addn-hosts=' /etc/dnsmasq.conf | head -n1 | sed -e 's/^.*\?=\s*\(.*\?\)\s*$/\1/')
 if test -f ${HOSTS_FILE}; then
 	echo "Watching ${HOSTS_FILE} for changes"
