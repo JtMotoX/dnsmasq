f182443cc71b9e3b8920e0d5ae31047909a1881a
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2021-06-06 21:07:33 -0700

    adding loglevel to container stdout

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,21 +7,21 @@ dnsmasq --test
 CONFIG_FILE="/etc/dnsmasq.conf"
 while inotifywait -e close_write ${CONFIG_FILE} >/dev/null; do
 	if ! dnsmasq --test >/dev/null; then
-		echo "Not reloading config since dnsmasq config is invalid"
+		echo "[INFO] Not reloading config since dnsmasq config is invalid"
 	else
-		(ps -ef | grep [d]nsmasq | awk '{print $1}' | xargs kill -1) && echo "Reloaded config"
+		(ps -ef | grep [d]nsmasq | awk '{print $1}' | xargs kill -1) && echo "[INFO] Reloaded config"
 	fi
 done &
 
 # RELOAD IF HOSTS FILE WAS MODIFIED
 HOSTS_FILE=$(grep '^addn-hosts=' /etc/dnsmasq.conf | head -n1 | sed -e 's/^.*\?=\s*\(.*\?\)\s*$/\1/')
 if test -f ${HOSTS_FILE}; then
-	echo "Watching ${HOSTS_FILE} for changes"
+	echo "[INFO] Watching ${HOSTS_FILE} for changes"
 	while inotifywait -e close_write ${HOSTS_FILE} >/dev/null; do
 		if ! dnsmasq --test >/dev/null; then
-			echo "Not reloading hosts records since dnsmasq config is invalid"
+			echo "[INFO] Not reloading hosts records since dnsmasq config is invalid"
 		else
-			(ps -ef | grep [d]nsmasq | awk '{print $1}' | xargs kill -1) && echo "Reloaded hosts records"
+			(ps -ef | grep [d]nsmasq | awk '{print $1}' | xargs kill -1) && echo "[INFO] Reloaded hosts records"
 		fi
 	done &
 fi
